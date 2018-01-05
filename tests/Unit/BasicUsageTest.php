6fa1c0e52969d8f63475ecba0afff1838fb4a217
Jordan Hall <jordan.hall@rapidweb.biz>
2018-01-05 16:51:29 +0000

    Reorganise tests

--- tests/Unit/BasicUsageTest.php
+++ tests/Unit/BasicUsageTest.php
@@ -9,15 +9,28 @@ class PoolIntegrationTest extends TestCase
         'DKK','HKD','HRK','HUF','IDR','ILS','INR','JPY','KRW','MXN','MYR','NOK','NZD','PHP',
         'PLN','RON','RUB','SEK','SGD','THB','TRY','USD','ZAR','EUR'];
 
-    public function testGBPRatesRetrieval()
+    public function testExpectedCurrenciesArePresent()
     {
         $rates = (new SimpleCurrencyRates())->get('GBP');
 
         foreach($this->expectedCurrencies as $expectedCurrency) {
             $this->assertTrue(array_key_exists($expectedCurrency, $rates), 'The expected currency code ('.$expectedCurrency.') is missing.');
         }
+        
+    }
+
+    public function testBaseCurrencyIsEqualToOne()
+    {
+        $rates = (new SimpleCurrencyRates())->get('GBP');
 
         $this->assertEquals(1, $rates['GBP'], 'Base currency does not equal 1.');
+    }
+
+    public function testRatesArrayIsFormattedCorrectly()
+    {
+        $rates = (new SimpleCurrencyRates())->get('GBP');
+
+        $this->assertTrue(is_array($rates));
 
         foreach($rates as $currency => $rate) {
             $this->assertTrue(is_string($currency), 'Currency code ('.$currency.') is not a string.');
@@ -25,6 +38,5 @@ class PoolIntegrationTest extends TestCase
             $this->assertTrue(is_float($rate), 'Currency rate (for '.$currency.') is not a float.');
             $this->assertTrue($rate>0, 'Currency rate (for '.$currency.') must be greater than zero.');
         }
-        
     }
 }
